ac14bbee67a8b46ad41dcaf2dc54cf805b3468c6
interviewDemo <95458867+interviewDemo@users.noreply.github.com>
2021-12-03 05:16:49 +0000

    Add files via upload

---
+++ setupsr.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# install updates, Nginx, Users, perms
+yum update -y
+yum install -y nginx
+systemctl start nginx
+systemctl enable nginx
+chown -R nginx:nginx /usr/share/nginx/www
+cd /usr/share/nginx/www
+git init
+git pull https://github.com/interviewDemo/interviewProject.git .
+useradd -m -p "gonzo" Alice
+useradd -m -p "kermit" Malory
+useradd -m -p "piggy" Bobby
+su Alice
+chmod 700 /usr/share/nginx/news
+su malory
+chmod 700 /usr/share/nginx/www/news
+su bobby
+chmod 700 /usr/share/nginx/www
