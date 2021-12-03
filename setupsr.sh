e8954bb62c9ecdd28c42602ac2449bd1d79752cc
interviewDemo <95458867+interviewDemo@users.noreply.github.com>
2021-12-03 05:23:06 +0000

    Add files via upload

--- setupsr.sh
+++ setupsr.sh
@@ -11,9 +11,12 @@ git pull https://github.com/interviewDemo/interviewProject.git .
 useradd -m -p "gonzo" Alice
 useradd -m -p "kermit" Malory
 useradd -m -p "piggy" Bobby
+useradd -m -p "chef" charlie
 su Alice
 chmod 700 /usr/share/nginx/news
 su malory
 chmod 700 /usr/share/nginx/www/news
 su bobby
 chmod 700 /usr/share/nginx/www
+su charlie chown -R charlie:charlie /people.html
+chmmod 700
